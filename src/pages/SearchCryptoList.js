6a74d74a81f4c02bb48224ba4548029c9d2f3560
Rajni Dua <rajni.dua14@gmail.com>
2021-12-08 23:34:05 +1030

    fetched 100 rows of data in get

--- src/pages/SearchCryptoList.js
+++ src/pages/SearchCryptoList.js
@@ -1,6 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const SearchCryptoList = () => {
+  const [coins, setcoins] = useState([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setcoins(res.data);
+        console.log(coins);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  /*   useEffect(() => {
+    if (error) {
+      alert("There has been an error fetching the data");
+    }
+  }, [error]); */
+
   return <div> My first crypto!!!</div>;
 };
 
