18bc1212d753f3b9ad337eff2339141c08a887f0
Rajni Dua <rajni.dua14@gmail.com>
2021-12-08 22:35:04 +1030

    initial setup with search crypto page and react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchCryptoList from "./pages/SearchCryptoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Switch>
+          <Route exact path="/" component={SearchCryptoList} />
+
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
---
+++ src/pages/SearchCryptoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchCryptoList = () => {
+  return <div> My first crypto!!!</div>;
+};
+
+export default SearchCryptoList;
