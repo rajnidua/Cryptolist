34046c7eb931dac2ee6ed6f4db0919e204877c25
Rajni Dua <rajni.dua14@gmail.com>
2021-12-09 14:15:54 +1030

    showing dynamic crypto list in table

--- src/components/SearchCrypto/ShowList.js
+++ src/components/SearchCrypto/ShowList.js
@@ -2,7 +2,42 @@ import React from "react";
 
 const ShowList = (props) => {
   console.log(props);
-  return <div> my crypto list </div>;
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>COIN</th>
+          <th>24hr</th>
+          <th>RATE</th>
+        </tr>
+        {props.filteredList.map((crypto) => (
+          <tr key={crypto.id}>
+            <td>
+              <div>
+                <img src={crypto.image} alt="image" />
+                {crypto.name}
+              </div>
+              <div>{crypto.symbol}</div>
+            </td>
+            <td>
+              {crypto.market_cap_change_percentage_24h < 0 ? (
+                <p className="red">
+                  {crypto.market_cap_change_percentage_24h.toFixed(2)}
+                  <span className="downArrow">&#x2193;</span>
+                </p>
+              ) : (
+                <p className="green">
+                  {crypto.market_cap_change_percentage_24h.toFixed(2)}
+                  <span className="upArrow">&#x2191;</span>
+                </p>
+              )}
+            </td>
+            <td>${crypto.current_price}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 };
 
 export default ShowList;
