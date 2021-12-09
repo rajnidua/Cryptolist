1b501081de6bab2267fa6c37c2c7aea4a7742356
Rajni Dua <rajni.dua14@gmail.com>
2021-12-09 20:51:50 +1030

    fixed css for list

--- src/components/SearchCrypto/ShowList.js
+++ src/components/SearchCrypto/ShowList.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "../../styles/showList.css";
 
 const ShowList = (props) => {
   console.log(props);
   return (
-    <div>
+    <div className="crypto-container">
+      {props.filteredList.map((crypto) => (
+        <div className="crypto-row" key={crypto.id}>
+          <div className="crypto-name">
+            <div className="img-section">
+              <img src={crypto.image} alt="image" />
+            </div>
+            <div className="name-section">
+              <div className="title">{crypto.name}</div>
+              <div>{crypto.symbol.toUpperCase()}</div>
+            </div>
+          </div>
+          <div className="change-percentage">
+            {crypto.market_cap_change_percentage_24h < 0 ? (
+              <p className="red">
+                {crypto.market_cap_change_percentage_24h.toFixed(2)}
+                <span className="down-arrow">&#x2193;</span>
+              </p>
+            ) : (
+              <p className="green">
+                {crypto.market_cap_change_percentage_24h.toFixed(2)}
+                <span className="up-arrow">&#x2191;</span>
+              </p>
+            )}
+          </div>
+          <div className="current-price">${crypto.current_price}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ShowList;
+
+/*<div>
       <table>
         <tr>
           <th>COIN</th>
@@ -13,11 +48,15 @@ const ShowList = (props) => {
         {props.filteredList.map((crypto) => (
           <tr key={crypto.id}>
             <td>
-              <div>
-                <img src={crypto.image} alt="image" />
-                {crypto.name}
+              <div className="name-container">
+                <div className="name-img">
+                  <img src={crypto.image} alt="image" />
+                </div>
+                <div className="name-info">
+                  <p>{crypto.name}</p>
+                  <div>{crypto.symbol}</div>
+                </div>
               </div>
-              <div>{crypto.symbol}</div>
             </td>
             <td>
               {crypto.market_cap_change_percentage_24h < 0 ? (
@@ -36,8 +75,4 @@ const ShowList = (props) => {
           </tr>
         ))}
       </table>
-    </div>
-  );
-};
-
-export default ShowList;
+    </div>*/
