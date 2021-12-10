e564f9494e8cdc9800139d1bbbd945011d8eb2cb
Rajni Dua <rajni.dua14@gmail.com>
2021-12-10 14:35:30 +1030

    fetching results on the basis of page in the api url

--- src/components/SearchCrypto/SearchCryptoBody.js
+++ src/components/SearchCrypto/SearchCryptoBody.js
@@ -9,24 +9,30 @@ import { Paginate } from "../../utils/Paginate";
 
 const SearchCryptoBody = () => {
   const [cryptos, setCryptos] = useState([]);
+  const [allPagesResponse, setAllPagesResponse] = useState([]);
+  const [cryptoPageNum, setCryptoPageNum] = useState(0);
   const [coinList, setCoinList] = useState([]);
   const [error, setError] = useState(false);
   const [searchInput, setSearchInput] = useState("");
   const [loading, setLoading] = useState("loading");
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log("value of search input is " + searchInput);
   };
   const handlePageChange = (page) => {
     console.log("the page change value is: ", page);
     setCurrentPage(page);
   };
 
-  const getData = async () => {
+  const loadMore = async () => {
+    console.log("value of crypto num is(loadMore): " + cryptoPageNum);
+    setCryptoPageNum(cryptoPageNum + 1);
     try {
-      const response = await searchCryptoList();
+      const response = await searchCryptoList(cryptoPageNum);
       setLoading("not loading");
       console.log(response.data);
       setCryptos(response.data);
@@ -38,6 +44,54 @@ const SearchCryptoBody = () => {
       console.log(err);
     }
   };
+  const loadFromBeginning = async () => {
+    console.log("value of crypto num is(loadfrom beginning): " + cryptoPageNum);
+    setCryptoPageNum(1);
+    try {
+      const response = await searchCryptoList(cryptoPageNum);
+      setLoading("not loading");
+      console.log(response.data);
+      setCryptos(response.data);
+
+      if (response.status !== 200) {
+        setError(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /* const getData = async () => {
+    for (let i = 1; i <= 4; i++) {
+      axios
+        .get(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${i}&sparkline=false`
+        )
+        .then((res) => {
+          setAllPagesResponse(res.data);
+          console.log("all pages response is ", allPagesResponse);
+        })
+        .catch((err) => console.error(err));
+      setCryptoPageNum(cryptoPageNum + 1);
+    }
+  };
+  if (cryptoPageNum === 4) {
+    setCryptos(allPagesResponse);
+  } */
+  /* const getData = async () => {
+    try {
+      const response = await searchCryptoList(cryptoPageNum);
+      setLoading("not loading");
+      console.log(response.data);
+      setCryptos(response.data);
+
+      if (response.status !== 200) {
+        setError(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }; */
   const cryptosLength = Object.keys(cryptos).length;
 
   const filteredList = searchInput
@@ -56,9 +110,9 @@ const SearchCryptoBody = () => {
       crypto.symbol.toLowerCase().includes(searchInput.toLowerCase())
   ); */
 
-  useEffect(() => {
+  /* useEffect(() => {
     getData();
-  }, []);
+  }, []); */
 
   /* useEffect(() => {
     axios
@@ -102,6 +156,36 @@ const SearchCryptoBody = () => {
           currentPage={currentPage}
           onPageChange={handlePageChange}
         />
+        {/*  {cryptoPageNum === 5 ? setCryptoPageNum(1) : ""}
+        <button onClick={loadFromBeginning}>Load from Beginning</button>
+        {cryptoPageNum <= 4 ? (
+          <button onClick={loadMore}>Load More(101-200)</button>
+        ) : (
+          ""
+        )} */}
+
+        {cryptoPageNum === 5 ? setCryptoPageNum(0) : ""}
+        {/*  {cryptoPageNum === 5 || cryptoPageNum === 1 ? (
+          <div>
+            <button onClick={loadFromBeginning}>Load from Beginning</button>
+          </div>
+        ) : (
+          ""
+        )} */}
+        {/* <button onClick={loadFromBeginning}>Load from Beginning</button> */}
+        {cryptoPageNum <= 4 ? (
+          <button onClick={loadMore}>
+            {cryptoPageNum === 4 ? (
+              <div>
+                <p>Load from start</p>
+              </div>
+            ) : (
+              <p>Load Next</p>
+            )}
+          </button>
+        ) : (
+          ""
+        )}
       </div>
 
       {/* {searchInput ? (
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const searchCryptoList = () => {
+export const searchCryptoList = (cryptoPageNum) => {
   return axios.get(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=3&sparkline=false"
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${cryptoPageNum}&sparkline=false`
   );
 };
